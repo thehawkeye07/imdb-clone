60995955102db787c3edca9509476be06f0b73af
Vaibhav Yergude <vaibhav.yergude@gmail.com>
2024-04-28 15:49:37 +0530

    resolved merge conflict

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import Card from "../card/Card.js";
 import "./movieList.css";
@@ -35,41 +34,3 @@ const MovieList = () => {
 };
 
 export default MovieList;
-=======
-import React, { useEffect, useState } from "react";
-import Card from "../card/Card.js";
-import "./movieList.css";
-import { useParams } from "react-router-dom";
-
-const MovieList = () => {
-  const [movieList, setMovieList] = useState([]);
-  const { type } = useParams();
-
-  useEffect(() => {
-    getData();
-  }, [type]);
-
-  const getData = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${
-        type ? type : "popular"
-      }?api_key=37be067c3def9a6226c600735773d1e0&language=en-US`
-    )
-      .then((res) => res.json())
-      .then((data) => setMovieList(data.results));
-  };
-
-  return (
-    <div className="movie__list">
-      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-      <div className="list__cards">
-        {movieList.map((movie) => (
-          <Card key={movie.id} movie={movie} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default MovieList;
->>>>>>> master
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -64,70 +63,3 @@ function Home() {
 }
 
 export default Home;
-=======
-import React, { useEffect, useState } from "react";
-import "./Home.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import { Link } from "react-router-dom";
-import MovieList from "../../movieList/movieList";
-
-function Home() {
-  const [popularMovies, setPopularMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=37be067c3def9a6226c600735773d1e0&language=en-US"
-    )
-      .then((res) => res.json())
-      .then((data) => setPopularMovies(data.results));
-  }, []);
-
-  return (
-    <div className="poster">
-      <Carousel
-        showThumbs={false}
-        autoPlay={true}
-        transitionTime={3}
-        infiniteLoop={true}
-        showStatus={false}
-      >
-        {popularMovies.map((movie) => (
-          <Link
-            style={{ textDecoration: "none", color: "white" }}
-            to={`/movie/${movie.id}`}
-          >
-            <div className="posterImage">
-              <img
-                src={`https://image.tmdb.org/t/p/original/${
-                  movie && movie.backdrop_path
-                }`}
-                alt="img"
-              ></img>
-            </div>
-            <div className="posterImage__overlay">
-              <div className="posterImage__title">
-                {movie ? movie.title : ""}
-              </div>
-              <div className="posterImage__runtime">
-                {movie ? movie.release_date : ""}
-                <span className="posterImage__rating">
-                  {movie ? movie.vote_average : ""}
-                  <i className="fas fa-star" />
-                  {""}
-                </span>
-              </div>
-              <div className="posterImage__description">
-                {movie ? movie.overview : ""}
-              </div>
-            </div>
-          </Link>
-        ))}
-      </Carousel>
-      <MovieList />
-    </div>
-  );
-}
-
-export default Home;
->>>>>>> master
